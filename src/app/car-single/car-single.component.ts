ec166eddc5aeda5e0613339abf8058c780b6f4f9
Bohdan <bohdanpolishchuk@gmail.com>
2018-06-25 17:43:08 +0300

    add buy, delete, add car

--- src/app/car-single/car-single.component.ts
+++ src/app/car-single/car-single.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Car } from '../car.model';
 
 @Component({
   selector: 'app-car-single',
   templateUrl: './car-single.component.html',
   styleUrls: ['./car-single.component.css']
 })
-export class CarSingleComponent implements OnInit {
+export class CarSingleComponent {
 
-  constructor() { }
+  @Input() car: Car;
+  @Output() deleteCar = new EventEmitter<Car> ()
 
-  ngOnInit() {
+  onDelete() {
+    this.deleteCar.emit(this.car);
   }
+
+  onBuy() {
+    this.car.isSold = true;
+  }
+
 
 }
--- src/app/cars-form/cars-form.component.ts
+++ src/app/cars-form/cars-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Car } from '../car.model';
+import * as moment from 'moment'
 
 @Component({
   selector: 'app-cars-form',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cars-form.component.css']
 })
 export class CarsFormComponent implements OnInit {
+  private id: number = 2;
+  carName: string = '';
+  carModel: string = '';
+  @Output() addCar = new EventEmitter<Car>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onAdd() {
+    if(this.carModel === '' || this.carName === '')
+      return this.id = ++this.id;
+    const car = new Car(
+      this.carName,
+      moment().format('DD.MM.YY'),
+      this.carModel,
+      false,
+      this.id
+    );
+    // debugger cause without @Output didn`t work
+    this.addCar.emit(car);
+
+    this.carName = '';
+    this.carModel = '';
+  }
+
+  onLoad() {
+    // todo
+  }
 }
